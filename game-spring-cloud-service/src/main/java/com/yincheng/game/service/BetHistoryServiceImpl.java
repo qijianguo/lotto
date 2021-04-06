ec0d02a33285a4dafaf29a8d6087499316809e47
qijianguo <1633972602@qq.com>
2021-04-06 16:02:03 +0800

    重复计算修复

--- game-spring-cloud-service/src/main/java/com/yincheng/game/service/BetHistoryServiceImpl.java
+++ game-spring-cloud-service/src/main/java/com/yincheng/game/service/BetHistoryServiceImpl.java
@@ -82,14 +82,14 @@ public class BetHistoryServiceImpl extends ServiceImpl<BetHistoryMapper, BetHist
             }
             latch.countDown();
         }));
-        list.forEach(bet -> ThreadPoolUtils.execute(() -> {
+        /*list.forEach(bet -> ThreadPoolUtils.execute(() -> {
             Account account = betHistoryService.settle(bet, result);
             if (notice && account != null) {
                 webSocketService.send(String.valueOf(account.getUserId()), Destination.account(), new RewardResponse(account));
                 notificationService.reward(new NotificationReq(account.getUserId(), "Rp" + account.getReward() + " in " + gameName.toUpperCase()));
             }
             latch.countDown();
-        }));
+        }));*/
         try {
             latch.await();
         } catch (InterruptedException e) {
