998a89b94e5b1fd4fc93cc14b87fe8d4838ce324
qijianguo <1633972602@qq.com>
2021-04-21 17:09:57 +0800

    添加分布式锁 多线程操作用户账户采用分布式锁

---
+++ game-spring-cloud-web/src/main/java/com/yincheng/game/web/interceptor/LockMethodInterceptor.java
@@ -0,0 +1,67 @@
+package com.yincheng.game.web.interceptor;
+
+import com.yincheng.game.common.exception.BusinessException;
+import com.yincheng.game.common.exception.EmBusinessError;
+import com.yincheng.game.model.anno.CacheLock;
+import com.yincheng.game.service.CacheKeyGenerator;
+import com.yincheng.game.service.RedisLockHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+/**
+ * redis 方案
+ * @author qijianguo
+ */
+@Aspect
+@Configuration
+@Slf4j
+public class LockMethodInterceptor {
+
+    private final RedisLockHelper redisLockHelper;
+    private final CacheKeyGenerator cacheKeyGenerator;
+
+    public LockMethodInterceptor(RedisLockHelper redisLockHelper, CacheKeyGenerator cacheKeyGenerator) {
+        this.redisLockHelper = redisLockHelper;
+        this.cacheKeyGenerator = cacheKeyGenerator;
+    }
+
+    @Around("execution(public * *(..)) && @annotation(com.yincheng.game.model.anno.CacheLock)")
+    public Object interceptor(ProceedingJoinPoint pjp) {
+        log.info("获取锁");
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        Method method = signature.getMethod();
+        CacheLock lock = method.getAnnotation(CacheLock.class);
+        if (StringUtils.isEmpty(lock.prefix())) {
+            throw new RuntimeException("lock key don't null...");
+        }
+        final String lockKey = cacheKeyGenerator.getLockKey(pjp);
+        String value = UUID.randomUUID().toString();
+        try {
+            // 假设上锁成功，但是设置过期时间失效，以后拿到的都是 false
+            final boolean success = redisLockHelper.lock(lockKey, value, lock.expire(), lock.timeUnit());
+            if (!success) {
+                throw new BusinessException(EmBusinessError.REPEAT_COMMIT_ERROR);
+            }
+            try {
+                return pjp.proceed();
+            } catch (Throwable e) {
+                if (e instanceof BusinessException) {
+                    throw (BusinessException)e;
+                }
+                log.error(e.getMessage(), e);
+                throw new BusinessException(EmBusinessError.UNKNOW_ERROR);
+            }
+        } finally {
+            redisLockHelper.unlock(lockKey, value);
+            log.info("释放锁");
+        }
+    }
+}
