87fd01117c28ca833f7680ead3b1728934b4d3ed
qijianguo <1633972602@qq.com>
2021-04-23 10:11:09 +0800

    1. 添加数字处理工具类

---
+++ game-spring-cloud-model/src/main/java/com/yincheng/game/model/enums/NotificationType.java
@@ -0,0 +1,21 @@
+package com.yincheng.game.model.enums;
+
+import lombok.Getter;
+
+/**
+ * @author qijianguo
+ */
+
+@Getter
+public enum NotificationType {
+
+    REWARD(1),
+
+    WITHDRAW(2);
+
+    private Integer type;
+
+    NotificationType(Integer type) {
+        this.type = type;
+    }
+}
